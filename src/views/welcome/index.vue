b28eb7d48117ef8a62a1d722712d5e24289fd835
xiaomo <wegi866@gmail.com>
2024-11-07 00:38:05 -0800

    Initial commit

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({
+  name: "Welcome"
+});
+</script>
+
+<template>
+  <h1>为了 come</h1>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import { getPluginsList } from "./build/plugins";
+import { include, exclude } from "./build/optimize";
+import { type UserConfigExport, type ConfigEnv, loadEnv } from "vite";
+import {
+  root,
+  alias,
+  wrapperEnv,
+  pathResolve,
+  __APP_INFO__
+} from "./build/utils";
+
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  const { VITE_CDN, VITE_PORT, VITE_COMPRESSION, VITE_PUBLIC_PATH } =
+    wrapperEnv(loadEnv(mode, root));
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias
+    },
+    // 服务端渲染
+    server: {
+      // 端口号
+      port: VITE_PORT,
+      host: "0.0.0.0",
+      proxy: {},
+      warmup: {
+        clientFiles: ["./index.html", "./src/{views,components}/*"]
+      }
+    },
+    plugins: getPluginsList(VITE_CDN, VITE_COMPRESSION),
+    optimizeDeps: {
+      include,
+      exclude
+    },
+    build: {
+      target: "es2015",
+      sourcemap: false,
+      chunkSizeWarningLimit: 4000,
+      rollupOptions: {
+        input: {
+          index: pathResolve("./index.html", import.meta.url)
+        },
+        output: {
+          chunkFileNames: "static/js/[name]-[hash].js",
+          entryFileNames: "static/js/[name]-[hash].js",
+          assetFileNames: "static/[ext]/[name]-[hash].[ext]"
+        }
+      }
+    },
+    define: {
+      __INTLIFY_PROD_DEVTOOLS__: false,
+      __APP_INFO__: JSON.stringify(__APP_INFO__)
+    }
+  };
+};
